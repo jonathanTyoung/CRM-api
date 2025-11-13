a5b8afae86fbdf3e57709fd09f1392df295c02f0
Jon Young <youngt.jonathan>
2025-11-12 20:47:35 -0600

    models can be migrated successfully

--- api/models/orders.py
+++ api/models/orders.py
@@ -2,13 +2,6 @@ from django.db import models
 from .agent_profiles import AgentProfile
 from .leads import Lead
 
-class AddOn(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
 class Order(models.Model):
     agent = models.ForeignKey(AgentProfile, on_delete=models.CASCADE, related_name='orders')
     lead = models.ForeignKey(Lead, on_delete=models.CASCADE, related_name='orders')
